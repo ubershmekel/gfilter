8459a599f7537314c7ebb8df9ee2c611b61911e7
Yuval Greenfield <ubershmekel@gmail.com>
2015-11-22 23:54:29 -0800

    Rowcharts for few distinct values

--- assets/gfilter.js
+++ assets/gfilter.js
@@ -1,60 +1,94 @@
-var gfilter = function(data) {
+/* global dc */
+/* global crossfilter */
+var gfilter = function (data, rootElement) {
     gfilter.removeAll();
-    gfilter.draw(data);
+    gfilter.init(data, rootElement);
 };
 
 gfilter.className = "gfilter";
+gfilter.width = 500;
+gfilter.height = 300;
 
-gfilter.removeAll = function() {
+gfilter.removeAll = function () {
     // remove old
     d3.selectAll("." + gfilter.className).remove();
-}
+};
 
-gfilter.addData = function(data) {
-    gfilter.crossfilter.add(rows);
+gfilter.addData = function (data) {
+    gfilter.crossfilter.add(data);
     dc.redrawAll();
-}
+};
 
-gfilter.draw = function(spendData) {
-    var isNumeric = function(n) {
+gfilter.init = function (data, rootElement) {
+    var isNumeric = function (n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
-    var addGraph = function (id) {
+    var addDiv = function (id) {
         var div = document.createElement("div");
         div.id = id;
         div.className = gfilter.className;
-        document.body.appendChild(div);
+        rootElement.appendChild(div);
+        return div;
     }
-    
-    var params = Object.keys(spendData[0]);
-    var charts = [];
-    var ndx = crossfilter(spendData);
+
+    var addText = function (text, parentDiv, cls) {
+        var textNode = document.createTextNode(text);
+        var line = document.createElement("div");
+        line.className = cls;
+        line.appendChild(textNode);
+        parentDiv.appendChild(line);
+    }
+
+    var complaintsDiv = addDiv("complaints");
+    var params = Object.keys(data[0]);
+    var ndx = crossfilter(data);
     gfilter.crossfilter = ndx;
+
+    var failedColumns = [];
+
     for (var i = 0; i < params.length; i++) {
         var propName = params[i];
         var chartId = "chart-hist-" + propName;
-        addGraph(chartId);
-        if (isNumeric(spendData[0][propName])) {
-            var theChart = dc.barChart("#" + chartId);
-            charts.push(theChart);
+        var chartDiv = addDiv(chartId);
+        var dim = ndx.dimension(function (d) { return d[propName]; });
 
-            var dim = ndx.dimension(function (d) { return d[propName]; });
+        if (isNumeric(data[0][propName])) {
+            var theChart = dc.barChart("#" + chartId);
+
             //var spendPerName = dim.group().reduceSum(function(d) {return d[propName];});
             var spendPerName = dim.group().reduceCount();
-            var minMax = d3.extent(spendData, function (d) { return +d[propName] });
+            var minMax = d3.extent(data, function (d) { return +d[propName] });
             var min = +minMax[0];
             var max = +minMax[1];
             theChart
                 .xAxisLabel(propName)
-                .width(500).height(300)
+                .width(gfilter.width).height(gfilter.height)
                 .dimension(dim)
                 .group(spendPerName)
                 .x(d3.scale.linear().domain([min, max]))
                 .elasticY(true);
             theChart.yAxis().ticks(2);
+        } else {
+            var uniques = d3.map(data, function (d) { return d[propName] });
+            // arbitrary amount that looks ok on the rowChart
+            if (uniques.size() < 21) {
+                var group = dim.group().reduceSum(function (d) { return d[propName]; });
+                var theChart = dc.rowChart("#" + chartId);
+                theChart
+                    .width(gfilter.width).height(gfilter.height)
+                    .dimension(dim)
+                    .group(group)
+                    .elasticX(true);
+                addText(propName, chartDiv, "chartTitle")
+            } else {
+                failedColumns.push(propName);
+            }
         }
     }
+
+    if (failedColumns.length > 0)
+        addText("Did not create chart for the columns: " + failedColumns.join(", "), complaintsDiv, "complaint");
 
     dc.renderAll();
 };
@@ -74,10 +108,7 @@ gfilter.draw = function(spendData) {
             })
             .on("read", function (files) {
                 var dataArray = files[0].data;
-                if(dataArray === undefined)
-                    // page refresh after a file was dropped
-                    return;
-                gfilter(dataArray);
+                gfilter(dataArray, document.body);
             }));
 })();
 
